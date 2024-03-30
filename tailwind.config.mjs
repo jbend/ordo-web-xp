2337b4c93ac67e3aaa892d66cc6101a50ad71069
Jeff Bendixsen <jeff_bendixsen@trimble.com>
2024-03-30 13:42:59 -0700

    Adding Catppuccin theme

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,14 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		darkMode: ["class", '[data-theme="dark"]'],
+		extend: {
+			colors: {
+				"base": "rgb(var(--color-base) / <alpha-value>)",
+				"text": "rgb(var(--color-text) / <alpha-value>)",
+			},
+		},
 	},
 	plugins: [],
 }
+
